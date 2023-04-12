9f4cff41f10e6d204191e61187103e142b29fd47
Georgiy Orlov <smmizland@gmail.com>
2023-04-12 19:16:03 +0300

    Базовые локали

---
+++ lang/en/messages.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'store' => [
+        'success' => 'New entry successfully created',
+        'exists' => 'This entry already exists.',
+    ],
+    'destroy' => [
+        'success' => 'Successfully removed',
+        'fail' => 'Deletion error',
+    ],
+    'update' => [
+        'success' => 'Successfully updated',
+        'fail' => 'Update error',
+    ],
+    'device' => [
+        'logout' => 'Logging out of the device was successful',
+        'not_found' => 'Device not found',
+    ],
+    'code' => [
+        'sent' => 'Code sent',
+        'fail' => 'Error submitting code',
+        'valid' => 'The code is correct',
+    ],
+];
---
+++ lang/ru/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Указаны некорректные данные.',
+    'password' => 'Неверный пароль.',
+    'throttle' => 'Слишком много попыток. Пожалуйста, попробуйте снова через :seconds секунд.',
+];
