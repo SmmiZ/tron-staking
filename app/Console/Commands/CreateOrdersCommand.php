481b8d82138c89a8065e79768a56ffd53e8fe380
Georgiy Orlov <smmizland@gmail.com>
2023-04-17 12:22:06 +0300

    Ежедневное создание заказов потребителей

---
+++ app/Console/Commands/CreateOrdersCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Consumer;
+use Illuminate\Console\Command;
+
+class CreateOrdersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-orders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Создание заказов по текущим потребителям';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Consumer::query()->where('amount', '>', 0)->chunk(100, function ($consumers) {
+            foreach ($consumers as $consumer) {
+                $consumer->orders()->create(['amount' => $consumer->amount]);
+            }
+        });
+    }
+}
