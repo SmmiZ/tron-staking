2e69755511060dc8f9c6bd049e302581d5e59da7
Georgiy Orlov <smmizland@gmail.com>
2023-04-18 18:20:54 +0300

    Сидер базового потребителя

---
+++ database/seeders/ConsumerSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\{Consumer, Order};
+use Illuminate\Database\Seeder;
+
+class ConsumerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $consumer = Consumer::query()->create([
+            'name' => 'Main consumer',
+            'address' => config('app.hot_spot_wallet') ?? 'address',
+            'resource_amount' => 30,
+        ]);
+
+        Order::query()->create([
+            'consumer_id' => $consumer->id,
+            'resource_amount' => 30,
+        ]);
+    }
+}
