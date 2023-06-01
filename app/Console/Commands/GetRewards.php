3ab1eed0ae332955f6ab090a1d43de1bf9f8ad1b
Georgiy Orlov <smmizland@gmail.com>
2023-06-01 13:01:12 +0300

    Награды. Доп. обработка исключений

--- app/Console/Commands/GetRewards.php
+++ app/Console/Commands/GetRewards.php
@@ -4,8 +4,11 @@ namespace App\Console\Commands;
 
 use App\Models\Wallet;
 use App\Services\StakeService;
+use App\Services\TronApi\Exception\TronException;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Sleep;
+use Throwable;
 
 class GetRewards extends Command
 {
@@ -28,12 +31,21 @@ class GetRewards extends Command
      */
     public function handle()
     {
-        Wallet::query()->orderBy('id')->chunk(50, function ($wallets) {
+        Wallet::query()->chunkById(50, function ($wallets) {
             foreach ($wallets as $wallet) {
-                (new StakeService($wallet))->getReward();
-
-                Sleep::for(config('app.sleep_ms'))->milliseconds();
+                try {
+                    (new StakeService($wallet))->getReward();
+                } catch (TronException|Throwable $e) {
+                    $this->error($e->getMessage());
+                    Log::critical('Error while claim reward', [
+                        'wallet_id' => $wallet->id,
+                        'error' => $e,
+                    ]);
+                } finally {
+                    Sleep::for(config('app.sleep_ms'))->milliseconds();
+                }
             }
+            sleep(1);
         });
     }
 }
